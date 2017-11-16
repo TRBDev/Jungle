3456f7606467bcbedfcd005fd4b8e055ccc3e4f2
AlexPerezDeveloper <alexperez.developer@gmail.com>
2017-11-16 21:12:30 +0100

    añadido estilos semantic

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+
+ <div class="login">
+    <h3>Iniciar Sesión</h3>
+
+    <div class="ui corner labeled input">
+      <input  type="text" v-model="email" placeholder="Email">
+      <div class="ui corner label">
+        <i class="asterisk icon"></i>
+      </div>
+    </div>
+    <div class="ui corner labeled input">
+      <input type="password" v-model="password" placeholder="Password">
+      <div class="ui corner label">
+        <i class="asterisk icon"></i>
+      </div>
+    </div>
+    <button class="ui teal basic button" v-on:click="signIn">Acceder</button>
+  </div>
+
+</template>
+<script> 
+
+ import firebase from 'firebase'
+
+  export default {
+    name: 'login',
+    data: function() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      signIn: function() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          (user) => {
+            this.$router.replace('hello')
+          },
+          (err) => {
+            alert('Error al acceder: ' + err.message)
+          }
+        );
+      }
+    }
+  }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,18 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Vuefire from 'vuefire';
 import router from './router'
-import Firebase from 'firebase';
+import firebase from 'firebase'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(Vuefire);
+let app;
+let config = {
+    apiKey: "AIzaSyAo-IvF63iCWi6zpPgIdgLNcyPISKdt2RE",
+    authDomain: "jungle-637d8.firebaseapp.com",
+    databaseURL: "https://jungle-637d8.firebaseio.com",
+    projectId: "jungle-637d8",
+    storageBucket: "jungle-637d8.appspot.com",
+    messagingSenderId: "671808464311"
+  };
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: { App }
-})
+firebase.initializeApp(config)
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      router
+    })
+  }
+});
